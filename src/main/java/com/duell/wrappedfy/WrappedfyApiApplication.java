efa6b43d2437775fe89976f92bfb67f56c539d50
Duell Galeno <84858185+EmanuelGaleno@users.noreply.github.com>
2026-06-07 11:24:19 -0300

    chore: initial commit wrappedfy

---
+++ src/main/java/com/duell/wrappedfy/WrappedfyApiApplication.java
@@ -0,0 +1,13 @@
+package com.duell.wrappedfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrappedfyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrappedfyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duell/wrappedfy/WrappedfyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duell.wrappedfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrappedfyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
